9d0e4c8bc53c2028e7b06274be08d09d618016f9
beep112 <88689885+beep112@users.noreply.github.com>
2024-08-12 18:28:53 -0400

    Finished the wireframe of the 3D renderer

--- Renderer/src/Viewer/Viewer.java
+++ Renderer/src/Viewer/Viewer.java
@@ -8,11 +8,74 @@
  */
 package Viewer;
 import javax.swing.*;
+
 import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.List;
 /**
  * 
  */
 public class Viewer {
+    
+    private static class Vertex {
+        private double x;
+        private double y;
+        private double z;
+        
+        
+        public Vertex(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+    
+    private static class Triangle {
+        Vertex x1;
+        Vertex x2;
+        Vertex x3;
+        Color color;
+        
+        public Triangle(Vertex x1, Vertex x2, Vertex x3, Color color) {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.x3 = x3;
+            this.color = color;
+        }
+    }
+    
+    private static class Matrix3 {
+        double[] values;
+        
+        public Matrix3(double[] values) {
+            this.values = values;
+        }
+        
+        public Matrix3 multiply(Matrix3 other) {
+            double[] result = new double[9];
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    for (int i = 0; i < 3; i++) {
+                        result[row * 3 + col] += this.values[row * 3 + i] * other.values[i * 3 + col];
+                    }
+                }
+            }
+            
+            return new Matrix3(result);
+        }
+        
+        public Vertex transform(Vertex in) {
+            return new Vertex(
+                    in.x * values[0] + in.y * values[3] + in.z * values[6],
+                    in.x * values[1] + in.y * values[4] + in.z * values[7], 
+                    in.x * values[2] + in.y * values[5] + in.z * values[8]
+            );
+                    
+        }
+    }
+    
+    
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         Container pane = frame.getContentPane();
@@ -33,11 +96,72 @@ public class Viewer {
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
+                
+                // creating the points and color for triangle
+                List<Triangle> tris = new ArrayList<>();
+                tris.add(new Triangle(new Vertex(100, 100, 100),
+                        new Vertex(-100, -100, 100),
+                        new Vertex(-100, 100, -100),
+                        Color.WHITE));
+                tris.add(new Triangle(new Vertex(100, 100, 100),
+                        new Vertex(-100, -100, 100),
+                        new Vertex(100, -100, -100),
+                        Color.RED));
+                tris.add(new Triangle(new Vertex(-100, 100, -100),
+                        new Vertex(100, -100, -100),
+                        new Vertex(100, 100, 100),
+                        Color.GREEN));
+                tris.add(new Triangle(new Vertex(-100, 100, -100),
+                        new Vertex(100, -100, -100),
+                        new Vertex(-100, -100, 100),
+                        Color.BLUE));
+                
+                // putting the triangle on the screen 
+                double heading = Math.toRadians(headingSlider.getValue());
+                // left right transformation matrix
+                Matrix3 transform = new Matrix3(new double[] {
+                        Math.cos(heading), 0, -Math.sin(heading),
+                        0, 1, 0,
+                        Math.sin(heading), 0, Math.cos(heading)
+                });
+                
+                // up down rotation matrix
+                Matrix3 headingTransform = new Matrix3(new double[] {
+                        Math.cos(heading), 0, Math.sin(heading),
+                        0, 1, 0,
+                        -Math.sin(heading), 0, Math.cos(heading)
+                });
+                double pitch = Math.toRadians(pitchSlider.getValue());
+                Matrix3 pitchTransform = new Matrix3(new double[] {
+                        1, 0, 0,
+                        0, Math.cos(pitch), Math.sin(pitch),
+                        0, -Math.sin(pitch), Math.cos(pitch)
+                });
+                transform = headingTransform.multiply(pitchTransform);
+                
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+                
+                for (Triangle t : tris) {
+                    Vertex x1 = transform.transform(t.x1);
+                    Vertex x2 = transform.transform(t.x2);
+                    Vertex x3 = transform.transform(t.x3);
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(x1.x, x1.y);
+                    path.lineTo(x2.x, x2.y);
+                    path.lineTo(x3.x, x3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
+                
             }
         };
         pane.add(renderPanel, BorderLayout.CENTER);
         
         frame.setSize(400, 400);
         frame.setVisible(true);
+        headingSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e -> renderPanel.repaint());
+        
     }
 }
